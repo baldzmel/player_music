ccaaa3746297927edb45a407d6385e5b3961a83a
baldzmel <melina.balduzzi@davinci.edu.ar>
2024-07-04 00:21:55 +0200

    [ADD/NEW] First commit: Create Component and Modules for HomePage. Setup project and configs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  name: string = 'Melina Baldz';
   title = 'playermusic-a';
+
 }
+
+
---
+++ src/app/shared/components/side-bar/side-bar.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-side-bar',
+  templateUrl: './side-bar.component.html',
+  styleUrls: ['./side-bar.component.css']
+})
+export class SideBarComponent {
+  linksMenu: Array<any> = [
+    {
+      name: 'Home',
+      icon: ''
+    },
+    {
+      name: 'Search',
+      icon: ''
+    },
+  ]
+}
